802129fe866395515f0b787cab80ca9fd4df99c6
Franklyn Rodriguez <92125381+FranklynRod@users.noreply.github.com>
2023-09-19 11:25:29 -0400

    Added the pagination buttons and the functionality for buttons.

--- js/script.js
+++ js/script.js
@@ -4,18 +4,7 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+// global scoped to allow it to be accessed in multiple functions
 const studentsPerPage = 9;
 
 const showPage = (list, page) => {
@@ -24,32 +13,52 @@ const showPage = (list, page) => {
 
   const studentList = document.querySelector('.student-list');
   studentList.innerHTML = "";
-
+   //loop through array of student objects to display nine on each page
   for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i <= endIndex){
       const html = ` <li class="student-item cf">
           <div class="student-details">
-            <img class="avatar" src="${list[i].picture}" alt="Photo of ${list[i].name}">
-            <h3>${list[i].name}</h3>
+            <img class="avatar" src="${list[i].picture.large}" alt="Photo of ${list[i].name.first} ${list[i].name.last}">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
             <span class="email">${list[i].email}</span>
           </div>
           <div class="joined-details">
-            <span class="date">${list[i].registered[0]}</span>
+            <span class="date">${list[i].registered.date}</span>
           </div>
         </li>`;
         studentList.insertAdjacentHTML("beforeend", html);
-        console.log(studentList)
       }
   }
 };
 
-console.log(showPage(data, 1));
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
 
 
-// Call functions
+const addPagination = (list) => {
+   const pageNum = Math.ceil(list.length / studentsPerPage);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   //create buttons and activated button of displayed page
+   for (let i = 1; i <= pageNum ; i++){
+      const html =  
+      `<li><button type="button">${i}</button>
+    </li>`
+    linkList.insertAdjacentHTML("beforeend", html);
+   };
+   linkList.querySelector("button").classList.add("active");
+   //Allows buttons to interactive
+   linkList.addEventListener("click",(e) => {
+      const activeButton = linkList.querySelector(".active");
+      const buttonClicked = e.target.closest("button");
+         if (buttonClicked){
+         activeButton.classList.remove("active")
+         buttonClicked.classList.add("active");
+         showPage(list, buttonClicked.textContent);
+         };
+
+   });
+  
+}
+
+showPage(data, 1);
+addPagination(data);
